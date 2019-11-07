88418e134c6245c215baa081130283ca7fad105f
p2327 <50136933+buckfizz@users.noreply.github.com>
2019-11-07 23:42:37 +0000

    updated utilities to follow pycodestyle

--- mltools/utilities.py
+++ mltools/utilities.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
-from pandas.api.types import is_string_dtype, is_numeric_dtype, is_categorical_dtype
+from pandas.api.types import (is_string_dtype, is_numeric_dtype,
+                              is_categorical_dtype)
 import re
 import sklearn
 from sklearn.preprocessing import StandardScaler
 from sklearn_pandas import DataFrameMapper
-from typing import *
+from typing import Callable, List, Union, Tuple
+import warnings
 
 
 def train_cats(df: pd.DataFrame) -> pd.DataFrame:
@@ -15,7 +17,7 @@ def train_cats(df: pd.DataFrame) -> pd.DataFrame:
 
     Parameters:
     -----------
-    df: a pandas dataframe. 
+    df: a pandas dataframe.
 
     Example usage:
     ---------
@@ -41,7 +43,8 @@ def train_cats(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def add_datepart(df: pd.DataFrame, date_field_name: str, drop: bool=True, time: bool=False) -> pd.DataFrame:
+def add_datepart(df: pd.DataFrame, date_field_name: str, drop: bool = True,
+                 time: bool = False) -> pd.DataFrame:
     """
     Helper function that manipulates datetime data.
     Adds columns relevant to discrete properties of a date.
@@ -52,57 +55,65 @@ def add_datepart(df: pd.DataFrame, date_field_name: str, drop: bool=True, time: 
     # initialise variables
     field = df[date_field_name]
     field_dtype = field.dtype
-    
+
     if isinstance(field_dtype, pd.core.dtypes.dtypes.DatetimeTZDtype):
         field_dtype = np.datetime64
-    
+
     if not np.issubdtype(field_dtype, np.datetime64):
-        df[date_field_name] = field = pd.to_datetime(field, infer_datetime_format=True)
-    
+        df[date_field_name] = field = pd.to_datetime(
+                                            field,
+                                            infer_datetime_format=True)
+
     # leaves dp and add attributes
-    #targ_pre = re.sub('[Dd]ate$', '', date_field_name)
-    attributes = ['Year', 
-                  'Month', 
-                  'Week', 
-                  'Day', 
-                  'Dayofweek', 
+    # targ_pre = re.sub('[Dd]ate$', '', date_field_name)
+    attributes = ['Year',
+                  'Month',
+                  'Week',
+                  'Day',
+                  'Dayofweek',
                   'Dayofyear',
-                  'Is_month_end', 
-                  'Is_month_start', 
-                  'Is_quarter_end', 
-                  'Is_quarter_start', 
-                  'Is_year_end', 
+                  'Is_month_end',
+                  'Is_month_start',
+                  'Is_quarter_end',
+                  'Is_quarter_start',
+                  'Is_year_end',
                   'Is_year_start']
-    
-    if time: 
+
+    if time:
         attributes = attributes + ['Hour', 'Minute', 'Second']
 
-    # creates new columns with pattern+attribute    
-    for attribute in attributes: 
+    # creates new columns with pattern+attribute
+    for attribute in attributes:
         df[date_field_name + attribute] = getattr(field.dt, attribute.lower())
-    
+
     df[date_field_name + 'Elapsed'] = field.astype(np.int64) // 10 ** 9
-    
-    if drop: 
+
+    if drop:
         df.drop(date_field_name, axis=1, inplace=True)
-    
+
     return df
 
 
-def remove_column_if_label_contains(df: pd.DataFrame, cols: List[str]) -> pd.DataFrame:
+def remove_column_if_label_contains(df: pd.DataFrame,
+                                    cols: List[str]) -> pd.DataFrame:
     """
-    Removes columns in a dataframe if name is partially matching any names in cols.
+    Removes features in a dataframe if a feature name
+    is partially matching any names in cols.
     """
     for name in cols:
-        df.drop([col for col in df.columns if name in col], axis=1, inplace=True)
+        df.drop([col for col in df.columns if name in col],
+                axis=1, inplace=True)
     return df
 
 
 # could add print for shape before and after
-def remove_row_if_column_contains(df: pd.DataFrame, label: str, value: Union[str, int, float]) -> pd.DataFrame:
+def remove_row_if_column_contains(df: pd.DataFrame,
+                                  label: str,
+                                  value: Union[str, int, float]
+                                  ) -> pd.DataFrame:
     """
     Removes a row from a dataframe where value is in column.
-    """ 
+    """
     index_names = df[df[label] == value].index
     df.drop(index_names, inplace=True)
     df.reset_index(inplace=True)
@@ -127,24 +138,27 @@ def make_dict(df: pd.DataFrame, key_column: str, value_column: str) -> dict:
     """
     return dict(zip(df[key_column].values, df[value_column].values))
 
+
 # encode new columns matching dictionary of values to list from dataframe
 def make_new_col(d: dict, keys: List[str]) -> List:
     """
-    Extract values from a dictionary in a list if exist a key match in passed keys list.
+    Extract values from a dictionary in a match exist in keys passed.
     This can be used to add new columns to a dataframe.
     """
-    # check if d[key] is actually checkign for a value and returns the key (the borough)
     return [d[key] for key in keys]
 
 
-def scale_vars(df: pd.DataFrame, mapper: DataFrameMapper=None) -> np.ndarray:
+def scale_vars(df: pd.DataFrame, mapper: DataFrameMapper = None) -> np.ndarray:
     """
     Returns a mapper to scale variables.
     """
-    warnings.filterwarnings('ignore', category=sklearn.exceptions.DataConversionWarning)
+    warnings.filterwarnings('ignore',
+                            category=sklearn.exceptions.DataConversionWarning)
     if mapper is None:
         # apply standardscaler to columns
-        map_f = [([column], StandardScaler()) for column in df.columns if is_numeric_dtype(df[column])]
+        map_f = [(
+            [column], StandardScaler()
+            ) for column in df.columns if is_numeric_dtype(df[column])]
         mapper = DataFrameMapper(map_f).fit(df)
     df[mapper.transformed_names_] = mapper.transform(df)
     return mapper
@@ -159,72 +173,80 @@ def get_sample(df: pd.DataFrame, n: int) -> pd.DataFrame:
     return df.iloc[idxs].copy()
 
 
-def proc_df(df: pd.DataFrame, y_fld: str=None, skip_flds: List[str]=None, ignore_flds: List[str]=None, do_scale: bool=False, na_dict: dict=None, 
-preproc_fn: Callable[[pd.DataFrame], pd.DataFrame]=None, max_n_cat: int=None, subset: int=None, mapper=None) -> Tuple[pd.DataFrame, np.ndarray, dict]:
+def proc_df(df: pd.DataFrame, y_fld: str = None,
+            skip_flds: List[str] = None, ignore_flds: List[str] = None,
+            do_scale: bool = False, na_dict: dict = None,
+            preproc_fn: Callable[[pd.DataFrame], pd.DataFrame] = None,
+            max_n_cat: int = None, subset: int = None,
+            mapper=None) -> Tuple[pd.DataFrame, np.ndarray, dict]:
     """
     Takes a dataframe and:
         Splits off the response variable
-        Changes the nonumeric df.dtypes into numeric. 
-    For each column which is not in skip_flds or in ignore_flds, 
+        Changes the nonumeric df.dtypes into numeric.
+    For each column which is not in skip_flds or in ignore_flds,
     NaN values are replaced by the median value of the column.
     """
-    if not ignore_flds: 
-        ignore_flds=[]
-        
-    if not skip_flds: 
-        skip_flds=[]
-        
-    if subset: 
+    if not ignore_flds:
+        ignore_flds = []
+
+    if not skip_flds:
+        skip_flds = []
+
+    if subset:
         df = get_sample(df, subset)
-    else: 
+    else:
         df = df.copy()
     ignored_flds = df.loc[:, ignore_flds]
     df.drop(ignore_flds, axis=1, inplace=True)
 
-    if preproc_fn: 
+    if preproc_fn:
         preproc_fn(df)
 
-    if y_fld is None: 
+    if y_fld is None:
         y = None
     else:
-        if not is_numeric_dtype(df[y_fld]): 
+        if not is_numeric_dtype(df[y_fld]):
             df[y_fld] = pd.Categorical(df[y_fld]).codes
         y = df[y_fld].values
         skip_flds += [y_fld]
     df.drop(skip_flds, axis=1, inplace=True)
 
-    if na_dict is None: 
+    if na_dict is None:
         na_dict = {}
-    else: 
+    else:
         na_dict = na_dict.copy()
     na_dict_initial = na_dict.copy()
 
-    for label, content in df.items(): 
+    for label, content in df.items():
         na_dict = fix_missing(df, content, label, na_dict)
-    
-    if len(na_dict_initial.keys()) > 0:
-        df.drop([a + '_na' for a in list(set(na_dict.keys()) - set(na_dict_initial.keys()))], axis=1, inplace=True)
 
-    if do_scale: 
+    if len(na_dict_initial.keys()) > 0:
+        df.drop([
+            a + '_na' for a in list(set(na_dict.keys()) -
+                                    set(na_dict_initial.keys()))
+            ], axis=1, inplace=True)
+
+    if do_scale:
         mapper = scale_vars(df, mapper)
 
-    for label, content in df.items(): 
+    for label, content in df.items():
         numericalize(df, content, label, max_n_cat)
-    
+
     df = pd.get_dummies(df, dummy_na=True)
     df = pd.concat([ignored_flds, df], axis=1)
     res = [df, y, na_dict]
-        
-    if do_scale: 
+
+    if do_scale:
         res = res + [mapper]
 
     return res
 
 
-def fix_missing(df: pd.DataFrame, col: pd.Series, target_label: str, na_dict: dict=None) -> dict:
+def fix_missing(df: pd.DataFrame, col: pd.Series,
+                target_label: str, na_dict: dict = None) -> dict:
     """
     Replaces na values with median if data is numeric.
-    Adds _na suffix columns where True means a NaN was replaced. 
+    Adds _na suffix columns where True means a NaN was replaced.
     Examples:
     ---------
     >>> df = pd.DataFrame({'col1' : [1, np.NaN, 3], 'col2' : [5, 2, 2]})
@@ -242,7 +264,7 @@ def fix_missing(df: pd.DataFrame, col: pd.Series, target_label: str, na_dict: di
     """
     # assumes a numerica dtype
     if na_dict is None:
-        na_dict={}
+        na_dict = {}
     if is_numeric_dtype(col):
         if pd.isnull(col).sum() or (target_label in na_dict):
             df[target_label+'_na'] = pd.isnull(col)
@@ -252,22 +274,24 @@ def fix_missing(df: pd.DataFrame, col: pd.Series, target_label: str, na_dict: di
     return na_dict
 
 
-def numericalize(df: pd.DataFrame, col: pd.Series, target_label: str, max_n_cat: int=None) -> pd.DataFrame:
+def numericalize(df: pd.DataFrame, col: pd.Series,
+                 target_label: str, max_n_cat: int = None) -> pd.DataFrame:
     """
     Converts non numeric values to numeric.
     """
     # codes+1 is to have NaN values set to 0 and not -1
-    if not is_numeric_dtype(col) and (max_n_cat is None or len(col.cat.categories)>max_n_cat):
+    if not is_numeric_dtype(col) and (max_n_cat is None or len(col.cat.categories) > max_n_cat):
         df[target_label] = pd.Categorical(col).codes+1
     return df
 
-# TODO 
-def split_vals(df, n): 
-    ''' 
+
+# TODO
+def split_vals(df, n):
+    '''
     Returns df copy of up to n and from n to end of the rows
     df[:n] for training and df[n:] for validation.
     '''
-    return df[:n].copy().reset_index(drop=True), df[n:].copy().reset_index(drop=True)
+    return df[:n].copy(), df[n:].copy()
 
 
 def peek(df):
@@ -281,16 +305,16 @@ def peek(df):
 
 def rf_feat_importance(m, df):
     return pd.DataFrame({
-        'cols':df.columns, 
-        'imp':m.feature_importances_
+        'cols': df.columns,
+        'imp': m.feature_importances_
         }).sort_values('imp', ascending=False)
 
 
-def plot_fi(features): 
-    return features.plot('cols', 
-                         'imp', 
-                         'barh', 
-                         figsize=(8,6),
-                         colormap='RdBu', 
+def plot_fi(features):
+    return features.plot('cols',
+                         'imp',
+                         'barh',
+                         figsize=(8, 6),
+                         colormap='RdBu',
                          legend=False
                          )
